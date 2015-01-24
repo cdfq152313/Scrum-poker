d7e0e19ba1b74fa858d3999fd3a519edc550b11a
denny <cdfq152313@gmail.com>
2015-01-25 01:29:23 +0800

    now is able to shift card

--- scrum_poker.py
+++ scrum_poker.py
@@ -4,16 +4,29 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 class Card(QLabel):
-    def __init__(self, title, parent):
-        super(Card, self).__init__("fibo\n" + title, parent)
+    prefix = "fibo\n"
+    def __init__(self, index,parent):
+        super(Card, self).__init__(self.prefix, parent)
+        self.register_screen(parent)
+        self.index = index
+        self.content = "" 
         palette1 = QPalette(self)
         # palette1.setColor(self.backgroundRole(), Qt.red  ) 
         palette1.setBrush(self.backgroundRole(), QBrush( QPixmap('./background/bg2.gif')))
+        # background = QPixmap("./poker/1.gif").scaled( Qsize(100,40) );
+        # palette1.setBrush(self.backgroundRole(), QBrush(background) ) 
         self.setAutoFillBackground(True)
         self.setPalette(palette1)
         self.setAcceptDrops(True)
         #self.setReadOnly(True)
 
+    def set_content(self, content):
+        self.content = content
+        self.setText(self.prefix + content)
+    def register_screen(self, screen):
+        self.screen=screen
+    def notify_screen(self):
+        self.screen.card_shift(self.index)
     def dragEnterEvent(self, e):
         if e.mimeData().hasFormat('text/plain'):
             e.accept()
@@ -21,30 +34,36 @@ class Card(QLabel):
             e.ignore()
 
     def dropEvent(self, e):
-        fibo = "fibo\n"
-        display = fibo + e.mimeData().text()
-        self.setText(display)
+        self.notify_screen()
+        self.set_content( e.mimeData().text() )
 
 class CardScreen(QWidget):
     x_max = 6
     y_max = 4
     card_max = x_max * y_max
-    card_count = 0
+    card = []
     def __init__(self, parent=None):
         super(CardScreen, self).__init__(parent)
         self.layout = QGridLayout()
 
-        self.new_card("hello world")
-        while self.card_count < self.card_max:
-            self.new_card("compiler作業")
+        card_count = 0
+        while card_count < self.card_max:
+            self.__init_card(card_count)
+            card_count += 1
 
         self.setLayout(self.layout)
 
-    def new_card(self, content):
-        aCard = Card(content, self)
-        (x,y) = self.__card_position(self.card_count)
+    def __init_card(self, index):
+        aCard = Card(index,self)
+        (x,y) = self.__card_position(index)
         self.layout.addWidget(aCard, y,x)
-        self.card_count += 1
+        self.card.append(aCard)
+
+    def card_shift(self, index):
+        cur = self.card_max-1
+        while cur > index:
+            self.card[cur].set_content( self.card[cur-1].content )
+            cur -= 1
 
     def __card_position(self, index):
         return ( index%self.x_max , index/self.x_max )
